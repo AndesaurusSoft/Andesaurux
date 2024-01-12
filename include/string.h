444b64caccd32709bc3f40679eb34712de2725b7
Anatoliy6463 <120381885+Anatoliy6463@users.noreply.github.com>
2024-01-12 13:46:54 +0300

    Create string.h

---
+++ include/string.h
@@ -0,0 +1,11 @@
+#ifndef STRING_H
+#define STRING_H
+#include "sizet.h"
+/*
+*   Copyright (C) 2024 Anatoliy6463
+*/
+int strcmp(const char* str1, const char* str2);
+char* strcpy(char* dest, const char* src);
+size_t strlen(const char* str);
+
+#endif // STRING_H
