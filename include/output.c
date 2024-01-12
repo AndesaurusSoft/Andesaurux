ff2aa132671fdca80b0649d0b28a3af595f09502
Anatoliy6463 <120381885+Anatoliy6463@users.noreply.github.com>
2024-01-12 13:50:36 +0300

    Create output.c

---
+++ include/output.c
@@ -0,0 +1,58 @@
+#include "stdio.h"
+#include "stdarg.h"
+/*
+*   Copyright (C) 2024 Anatoliy6463
+*/
+int printf(const char *format, ...)
+{
+        va_list args;
+        va_start(args, format);
+        __asm__("push %%fs\n\t"
+                        "push %%ds\n\t"
+                        "pop %%fs\n\t"
+                        "pushl %0\n\t"
+                        "pushl $format\n\t"
+                        "pushl $args\n\t"
+                        "call printf\n\t"
+                        "addl $12,%%esp\n\t"
+                        "popl %0\n\t"
+                        "pop %%fs"
+                        ::"r" (format):"ax","cx","dx");
+        va_end(args);
+}
+int puts(const char *str) {
+    int i = 0;
+    while (str[i] != '\0') {
+        putchar(str[i]);
+        i++;
+    }
+    putchar('\n');
+    return i;
+}
+int putchar(int c) {
+    __asm__("mov $0x02, %%eax\n\t"
+            "mov $0x01, %%ebx\n\t"
+            "movb %0, %%dl\n\t"
+            "int $0x80"
+            ::"r" (c)
+            :"%eax","%ebx","%edx");
+}
+int fputs(const char *str, FILE *f) {
+    int i = 0;
+    while (str[i] != '\0') {
+        fputc(str[i], f);
+        i++;
+    }
+}
+int fputc(int c, FILE *f) {
+    __asm__("mov $0x02, %%eax\n\t"
+            "mov $0x01, %%ebx\n\t"
+            "movb %0, %%dl\n\t"
+            "int $0x80"
+            ::"r" (c)
+            :"%eax","%ebx","%edx");
+}
+int putc(int c)
+{
+        putchar(c);
+}
