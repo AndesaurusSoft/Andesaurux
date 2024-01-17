3a88e784d3144e24a884d2db0c494faf8cc34588
Anatoliy6463 <120381885+Anatoliy6463@users.noreply.github.com>
2024-01-17 20:16:08 +0300

    Create gitcc.c

---
+++ gitcc.c
@@ -0,0 +1,20 @@
+/*
+*  gitcc means "Git cloner and compiler", it's not compatible with some programms, but i think, most of the programs will be compatible with this
+*/
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char** argv)
+{
+        char *url = argv[1];
+        char *command = "git clone ";
+        char *final = strcat(command, url);
+        puts("Cloning...\n");
+        system(final);
+        puts("Compiling...\n");
+        system("make");
+        puts("Moving to bin folder...\n");
+        system("make install");
+        puts("Done!\n");
+        return (0);
+}
