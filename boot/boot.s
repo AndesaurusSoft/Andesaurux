ad60850280b07e7d643a4cfa365928da23dd6526
Anatoliy6463 <novgorodov-12@mail.ru>
2024-02-18 20:30:07 +0300

    Bootloader

--- boot/boot.s
+++ boot/boot.s
@@ -1,7 +1,22 @@
-/*******************************
-* Bootloader for Andesaurux OS *
-* Written in GAS assembly      *
-********************************/
-.globl _start
-
-_start:
+.section .text
+    .code16
+    .org  0x7C00
+start:
+    mov  %cs, %ax
+    mov  %ax, %ds
+    call boot
+    cld
+    mov  $0x0E, %ah
+    mov  $0x00, %bh
+puts_loop:
+    lodsb
+    test %al, %al
+    jz   puts_loop_exit
+    int  $0x10
+    jmp  puts_loop
+puts_loop_exit:
+    jmp  puts_loop_exit
+.balign 512, 0
+finish:
+     .fill 510 - (finish - start), 1, 0
+     .byte  0x55, 0xAA
