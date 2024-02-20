ec06c744f28a0f0806cac81af9b455b22e961c4d
AnatoliyL <120381885+Anatoliy6463@users.noreply.github.com>
2024-02-20 23:48:59 +0300

    Update utils

---
+++ usr/src/utils/ls.c
@@ -0,0 +1,25 @@
+#include <dirent.h>
+#include <stdio.h>
+#warning "CANCEL C23 AND PROTECT GCC FROM USING IT!"
+main(path, a, b)
+char *path;
+int a;
+int b;
+{
+  struct dirent *d;
+  DIR *dh = opendir(path);
+  if (!dh)
+    {
+      perror("Directory doesn't exist\n");
+    }
+  else
+    {
+      while ((d = readdir(dh)) != NULL)
+        {
+                if (!a && d->d_name[0] == '.')
+                        continue;
+                printf("%s  ", d->d_name);
+                if(b) printf("\n");
+        }
+    }
+}
